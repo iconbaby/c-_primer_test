55f740994c6429c82a7455eec3906a96d494c215
enw <644187060@qq.com>
2017-02-17 10:33:24 +0800

    init the book of c++primer test project

---
+++ cmake-build-debug/CMakeFiles/c_primer_test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c_primer_test.dir/main.cpp.obj"
+  "c_primer_test.pdb"
+  "c_primer_test.exe"
+  "c_primer_test.exe.manifest"
+  "libc_primer_test.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/c_primer_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
